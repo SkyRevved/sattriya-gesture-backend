d34af9da1148aee8c8faca8c403e0e37a451ddac
Srinivash Baruah <srini.baruah@gmail.com>
2025-06-06 22:58:44 +0530

    Add MobileNetV2 based model and update training code

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
 import requests
 
-url = 'https://sattriya-gesture-backend.onrender.com/predict'
-file_path = r'C:\Projects\Temp\Testing\Dol\unscreen-076.png'
+url = "https://sattriya-gesture-backend.onrender.com/predict"
+file_path = r"C:\Projects\Temp\Testing\Dol\unscreen-076.png"
 
-with open(file_path, 'rb') as f:
-    files = {'file': f}
+with open(file_path, "rb") as f:
+    files = {"file": f}
     response = requests.post(url, files=files)
 
 print("Status Code:", response.status_code)
-print("Response Headers:", response.headers)
-print("Response Content:", response.text)  # print raw response
+try:
+    print("Response JSON:", response.json())
+except Exception as e:
+    print("Failed to parse JSON:", e)
+    print("Response Text:", response.text)
